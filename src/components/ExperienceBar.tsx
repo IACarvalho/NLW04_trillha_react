50efd43b47ab16d5857559bf04f4bad309e0c22f
IACarvalho <israelcarvalhodesenvolvedor@gmail.com>
2021-03-04 13:36:05 -0800

    NLW04 trilha React dia 03

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,15 +1,28 @@
+import { useContext } from "react";
+import { ChallengesContext } from "../Contexts/ChallengesContext";
 import styles from "../styles/components/ExperienceBar.module.css";
 
 export function ExperienceBar() {
+  const { currentExperience, experienceToNextLevel } = useContext(
+    ChallengesContext
+  );
+
+  const percentToNextLevel =
+    Math.round(currentExperience * 100) / experienceToNextLevel;
+
   return (
     <header className={styles.experienceBar}>
       <span>0 XP</span>
       <div>
-        <div style={{ width: "50%" }} />
-        <span style={{ left: "50%" }} className={styles.currentExperience}>
-          300 xp
+        <div style={{ width: `${percentToNextLevel}%` }} />
+        <span
+          style={{ left: `${percentToNextLevel}%` }}
+          className={styles.currentExperience}
+        >
+          {currentExperience} xp
         </span>
       </div>
+      <span>{experienceToNextLevel}</span>
     </header>
   );
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
 import "../styles/global.css";
+import { ChallengeProvider } from "../Contexts/ChallengesContext";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ChallengeProvider>
+      <Component {...pageProps} />
+    </ChallengeProvider>
+  );
 }
 
 export default MyApp;
